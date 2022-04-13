8ac68639cf06bf63cac16a898002531841bda1c5
JON ROBERT ARMSTRONG <JON.ROBERT.ARMSTRONG@GMAIL.COM>
2022-04-13 10:00:52 -0700

    SEPARATED LAB 06 AND LAB 07 JAVA SCRIPTS

--- js/constructor.js
+++ js/constructor.js
@@ -14,73 +14,110 @@
 
 // NOTE: Please use a header cell for both the header row ( containing store hours ), and the footer row ( hourly and grand totals across all stores ).
 
-function Location(title, minCust, maxCust, avgCookieSale){  // , randCookiesPerH, allCookiesSold, allCookiesSold, genRandCookiesPerH) { // Capital L because it is a constructor 
-    this.title = title;
-    this.minCust = minCust;
-    this.maxCust = maxCust;
-    this.avgCookieSale = avgCookieSale;
-}
+const hours = ['6am', '7am','8am', '9am', '10am', '11am','12am', '1pm', '2pm', '3pm','4pm', '5pm', '6pm', '7pm']
 
-const seattle = new Location('Seattle', 23, 65, 6.3,)
+function randomInRange(min, max){
+    const span = max - min + 1;   //check for 'off by one errors'
+    const randInSpan = Math.floor(Math.random() *span);
+    return min + randInSpan;
+};
 
+function Location(title, minCust, maxCust, avgCookieSale, randCookiesPerH, allCookiesSold, allCookiesSold, genRandCookiesPerH) { // Capital L because it is a constructor 
+    this.title = title;                     // parameter filled with argument (value?) in each constructor
+    this.minCust = minCust;                 // parameter filled with argument (value?) in each constructor
+    this.maxCust = maxCust;                 // parameter filled with argument (value?) in each constructor
+    this.avgCookieSale = avgCookieSale;     // parameter filled with argument (value?) in each constructor
+    this.randCookiesPerH = [];              // creates and array
+    this.allCookiesSold = 0;                // variable needed for later in genRandCookiesPerH function/method
+    this.genRandCookiesPerH();              // method that calls the function below for each location
+};
 
-const tokyo = new Location('Tokyo', 3, 24, 1.2,)
+//  method (function in an object that generates values) property of object
+Location.prototype.genRandCookiesPerH = function () {
+    let total = 0;                              // defines 'total' and sets value to 0.
+        console.log(`Code check: ${this.title} genRandCookiesPerH method started.`);
+        for (let i = 0; i < hours.length; i++) {    // declaring a for loop because I know how many times to repeat (once for each 'hour')
+            const randCustPerH = randomInRange(this.minCust, this.maxCust);
+            // declaring a new variable and filling with a random number(inclusive) from a set min/max range referenced from inside this same object.
+            const cookiesSold = Math.ceil(this.avgCookieSale * randCustPerH);  // The Math.ceil() function always rounds a number up to the next largest integer!
+            // declaring a new variable and filling with the total cookies sold based on average cookie sale and our random customer projection.
+            // const element = hours[index];        // did not use this path
+            this.randCookiesPerH[i] = cookiesSold;  // fills each indexed in the randCookiesPerH array with the number of cookies sold.
+            total += cookiesSold;                   // adds each run of the loop together for a grand total.
+        }
+        this.allCookiesSold = total;                // fills allCookiesSold in the object with the total from the getRandCookiesPerH method.
+        console.log(`Code check: ${this.title} genRandCookiesPerH method ended with a total value of "${total}".`);     // simple test
+};
 
-const dubai = new Location('Dubai', 11, 38, 3.7,)
+// render: function () {
 
-const paris = new Location('Paris', 20, 38, 2.3,)
+// //  Location	Min / Cust	Max / Cust	Avg Cookie / Sale
+// //  Inspired by John Cokos - Code 201 Day 07 Demo - 2022 Mar 20
+// //  These look like they could become for loops
+// const containerElem = document
 
-const lima = new Location('Lima', 2, 16, 4.6,)
+//   const table = document.createElement('table');
 
-const seattle = new Location('Seattle', 23, 65, 6.3,)
+//   const row1 = document.createElement('tr');
+//   const row2 = document.createElement('tr');
+//   const row3 = document.createElement('tr');
+//   const row4 = document.createElement('tr');
+//   const row5 = document.createElement('tr');
 
+//   const th1 = document.createElement('th');
+//   th1.textContent = 'Location';
+//   const th2 = document.createElement('th');
+//   th2.textContent = 'Min / Cust';
+//   const th3 = document.createElement('th');
+//   th3.textContent = 'Max / Cust';
+//   const th4 = document.createElement('th');
+//   th4.textContent = 'Cookie / Sale';
 
-render: function () {
+//   const td1 = document.createElement('td');
+//   td1.textContent = this.title;
+//   const td2 = document.createElement('td');
+//   td2.textContent = this.minCust;
+//   const td3 = document.createElement('td');
+//   td3.textContent = this.maxCust;
+//   const td4 = document.createElement('td');
+//   td4.textContent = this.avgCookieSale;
 
-//  Location	Min / Cust	Max / Cust	Avg Cookie / Sale
-//  Inspired by John Cokos - Code 201 Day 07 Demo - 2022 Mar 20
-//  These look like they could become for loops
+//   row1.appendChild(th1);
+//   row1.appendChild(th2);
+//   row1.appendChild(th3);
+//   row1.appendChild(th4);
 
-  const table = document.createElement('table');
+//   row2.appendChild(td1);
+//   row2.appendChild(td2);
+//   row2.appendChild(td3);
+//   row2.appendChild(td4);
 
-  const row1 = document.createElement('tr');
-  const row2 = document.createElement('tr');
-  const row3 = document.createElement('tr');
-  const row4 = document.createElement('tr');
-  const row5 = document.createElement('tr');
+//   table.appendChild(row1);
+//   table.appendChild(row2);
+//   table.appendChild(row3);
+//   table.appendChild(row4);
+//   table.appendChild(row5);
+//   article.appendChild(table);
+// }
+//   seattle.render();
+//   tokyo.render();
+//   dubai.render();
+//   paris.render();
+//   lima.render();
 
-  const th1 = document.createElement('th');
-  th1.textContent = 'Location';
-  const th2 = document.createElement('th');
-  th2.textContent = 'Min / Cust';
-  const th3 = document.createElement('th');
-  th3.textContent = 'Max / Cust';
-  const th4 = document.createElement('th');
-  th4.textContent = 'Cookie / Sale';
+// constructors will generally be put at the bottom even though JS will hoist things if necessary
 
-  const td1 = document.createElement('td');
-  td1.textContent = this.title;
-  const td2 = document.createElement('td');
-  td2.textContent = this.minCust;
-  const td3 = document.createElement('td');
-  td3.textContent = this.maxCust;
-  const td4 = document.createElement('td');
-  td4.textContent = this.avgCookieSale;
+const seattle = new Location('Seattle', 23, 65, 6.3);
+console.log(seattle);  // lower case
 
-  row1.appendChild(th1);
-  row1.appendChild(th2);
-  row1.appendChild(th3);
-  row1.appendChild(th4);
+const tokyo = new Location('Tokyo', 3, 24, 1.2);
+console.log(tokyo);
 
-  row2.appendChild(td1);
-  row2.appendChild(td2);
-  row2.appendChild(td3);
-  row2.appendChild(td4);
+const dubai = new Location('Dubai', 11, 38, 3.7);
+console.log(dubai);
 
-  table.appendChild(row1);
-  table.appendChild(row2);
-  table.appendChild(row3);
-  table.appendChild(row4);
-  table.appendChild(row5);
-  article.appendChild(table);
-}
+const paris = new Location('Paris', 20, 38, 2.3);
+console.log(paris);
+
+const lima = new Location('Lima', 2, 16, 4.6);
+console.log(lima);
