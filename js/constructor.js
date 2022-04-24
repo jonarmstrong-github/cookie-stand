7abe120178736218de7c81a0b99b70e48b29dc54
JON ROBERT ARMSTRONG <JON.ROBERT.ARMSTRONG@GMAIL.COM>
2022-04-23 22:10:25 -0700

    TABLES ARE BUILT AND FUNCTIONAL.

--- js/constructor.js
+++ js/constructor.js
@@ -1,29 +1,17 @@
-// Sales Data
-// Instructions
-// Create a new branch for today’s lab. Make sure it has all of your changes from lab 06 so that you can extend the functionality.
+'use strict'
 
-// Replace all of your object literals for the salmon cookie stand with a single constructor function that, when called with the ‘new’ keyword, it creates a new instance.
+const header = ['06:00', '07:00', '08:00', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00',]
+const hourlyTotals = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];  //there must be a better way to set this array as numbers for math later.
+var grandTotal = 0;
+const tableElem = document.createElement('table');  //used by multiple functions
 
-// Replace the lists of your data for each store and build a single table of data instead. It should look similar to the following:
-
-// Display each stores data in a table format similar to what is below. Break each column by the hour and complete each row with a “Daily Location Total”.
-
-// Each cookie stand location should have a separate render() method that creates and appends its row to the table
-
-// The header row and footer row are each created in their own stand-alone function
-
-// NOTE: Please use a header cell for both the header row ( containing store hours ), and the footer row ( hourly and grand totals across all stores ).
-
-const hours = ['06:00', '07:00','08:00', '09:00', '10:00', '11:00', '12:00',   //already defined in app.js
-                '13:00', '14:00', '15:00','16:00', '17:00', '18:00', '19:00']
-
-function randomInRange(min, max){
+function randomInRange(min, max) {
     const span = max - min + 1;
-    const randInSpan = Math.floor(Math.random() *span);
+    const randInSpan = Math.floor(Math.random() * span);
     return min + randInSpan;
 };
 
-function Location(title, minCust, maxCust, avgCookieSale, randCookiesPerH, allCookiesSold, allCookiesSold, genRandCookiesPerH) { // Capital L because it is a constructor 
+function Location(title, minCust, maxCust, avgCookieSale, randCookiesPerH, allCookiesSold, genRandCookiesPerH) { // Capital L because it is a constructor 
     this.title = title;                     // parameter filled with argument (value?) in each constructor
     this.minCust = minCust;                 // parameter filled with argument (value?) in each constructor
     this.maxCust = maxCust;                 // parameter filled with argument (value?) in each constructor
@@ -33,107 +21,218 @@ function Location(title, minCust, maxCust, avgCookieSale, randCookiesPerH, allCo
     this.genRandCookiesPerH();              // method that calls the function below for each location
 };
 
-//  method (function in an object that generates values) property of object
 Location.prototype.genRandCookiesPerH = function () { //cumulative daily total of a store
     let total = 0;
     console.log(`Code check: ${this.title} genRandCookiesPerH method started.`);
 
-        for (let i = 0; i < hours.length; i++) { 
-            const randCustPerH = randomInRange(this.minCust, this.maxCust);
-            const cookiesSold = Math.ceil(this.avgCookieSale * randCustPerH);
-            this.randCookiesPerH[i] = cookiesSold;
-            total += cookiesSold;
-        }
-
+    //HOURLY PREDICTIONS
+    for (let i = 0; i < header.length; i++) {
+        const randCustPerH = randomInRange(this.minCust, this.maxCust);
+        const cookiesSold = Math.ceil(this.avgCookieSale * randCustPerH);
+        this.randCookiesPerH[i] = cookiesSold;
+        total += cookiesSold;
+    }
     this.allCookiesSold = total;
     console.log(`Code check: ${this.title} genRandCookiesPerH method ended with a total value of "${total}".`);
+
+    //CUMULATIVE HOURLY TOTALS OF PREDICTIONS
+    for (let i = 0; i < header.length; i++) {
+        hourlyTotals[i] += this.randCookiesPerH[i];
+        grandTotal += hourlyTotals[i];
+    };
 };
-//  global function for grand total
-// let grandTotalPH = 0;
-//     console.log(`Code check: ${this.title} grandTotalPH method started.`);
-//         for (let i = 0; i < hours.length; i++) { 
-//             // const randCustPerH = randomInRange(this.minCust, this.maxCust);
-//             // const cookiesSold = Math.ceil(this.avgCookieSale * randCustPerH);
-//             // this.randCookiesPerH[i] = cookiesSold;
-//             grandTotalPH += cookiesSold;
-//         }
-//     this.allCookiesSold = total;
-//     console.log(`Code check: ${this.title} grandTotalPH method ended with a total value of "${total}".`);
-Location.prototype.render = function () {
 
 //  Trying to print constructor values for verification
 //  Location	Min / Cust	Max / Cust	Avg Cookie / Sale
 //  Inspired by John Cokos - Code 201 Day 07 Demo - 2022 Mar 20
-//  These look like they could become for loops
-const containerElem = document
 
-  const table = document.createElement('table');
+// STARTING POINT FOR TABLE OF PLANNING INPUTS
+const containerElem = document.getElementById('inputTable');    // sets the root container of the function to the section with 'location' id?
 
-  const row1 = document.createElement('tr');
-  const row2 = document.createElement('tr');
-  const row3 = document.createElement('tr');
-  const row4 = document.createElement('tr');
-  const row5 = document.createElement('tr');
+//CONSTANT FRAMEWORK OF TABLE OF PLANNING INPUTS
+const table = document.createElement('table');    //creates instructions to build a table
+containerElem.appendChild(table);                 //appends ta table element to the container
 
-  const th1 = document.createElement('th');
-  th1.textContent = 'Location';
-  const th2 = document.createElement('th');
-  th2.textContent = 'Min / Cust';
-  const th3 = document.createElement('th');
-  th3.textContent = 'Max / Cust';
-  const th4 = document.createElement('th');
-  th4.textContent = 'Cookie / Sale';
+const row1 = document.createElement('tr');
 
-  const td1 = document.createElement('td');
-  td1.textContent = this.title;
-  const td2 = document.createElement('td');
-  td2.textContent = this.minCust;
-  const td3 = document.createElement('td');
-  td3.textContent = this.maxCust;
-  const td4 = document.createElement('td');
-  td4.textContent = this.avgCookieSale;
+const th1 = document.createElement('th');
+th1.textContent = 'Location';
+const th2 = document.createElement('th');
+th2.textContent = 'Min / Cust';
+const th3 = document.createElement('th');
+th3.textContent = 'Max / Cust';
+const th4 = document.createElement('th');
+th4.textContent = 'Cookie / Sale';
 
-  row1.appendChild(th1);
-  row1.appendChild(th2);
-  row1.appendChild(th3);
-  row1.appendChild(th4);
+table.appendChild(row1);
 
-  row2.appendChild(td1);
-  row2.appendChild(td2);
-  row2.appendChild(td3);
-  row2.appendChild(td4);
+//VALUES OF PLANNING INPUTS
+Location.prototype.render2 = function () {     // This puts input data on the screen as a table! 
 
-  table.appendChild(row1);
-  table.appendChild(row2);
-  table.appendChild(row3);
-  table.appendChild(row4);
-  table.appendChild(row5);
-  article.appendChild(table);
-}
-//   seattle.render();
-//   tokyo.render();
-//   dubai.render();
-//   paris.render();
-//   lima.render();
+    const row2 = document.createElement('tr');
 
-// constructors will generally be put at the bottom even though JS will hoist things if necessary
+    const td1 = document.createElement('td');
+    td1.textContent = this.title;
+    const td2 = document.createElement('td');
+    td2.textContent = this.minCust;
+    const td3 = document.createElement('td');
+    td3.textContent = this.maxCust;
+    const td4 = document.createElement('td');
+    td4.textContent = this.avgCookieSale;
+
+    row1.appendChild(th1);
+    row1.appendChild(th2);
+    row1.appendChild(th3);
+    row1.appendChild(th4);
+
+    row2.appendChild(td1);
+    row2.appendChild(td2);
+    row2.appendChild(td3);
+    row2.appendChild(td4);
+
+    table.appendChild(row2);
+};
+
+//DAILY TABLE OF TOTALS
+//CREATES A TABLE WITH A HEADER ROW AND A FOOTER
+function createTable() {  //MIGHT NOT HAVE NEEDED TO BE A FUNCTION EXCEPT THAT I AM REUSING VARIABLES
+    const containerElem = document.getElementById('outputTable');
+
+    //NEW TABLE
+    containerElem.appendChild(tableElem);
+
+    //HEADER
+    const tableHead = document.createElement('thead');
+    tableElem.appendChild(tableHead);
+
+    //HEADER ROW
+    const headerRow = document.createElement('tr');
+    tableHead.appendChild(headerRow);
+
+    //HEADER DATA TAGS
+    const headerCell = document.createElement('th');
+    headerRow.appendChild(headerCell);
+
+    //INSERTS HOURS INTO HEADER TAGS
+    for (let i = 0; i < header.length; i++) {
+        const headerCell = document.createElement('th');
+        headerRow.appendChild(headerCell);
+        headerCell.textContent = header[i];
+    }
+
+    //APPENDS "TOTALS" ONTO THE END OF THE HEADER ROW.
+    const locTotal = document.createElement('th');
+    headerRow.appendChild(locTotal);
+    locTotal.textContent = 'Totals';
+
+    //FOOTER
+    const tableFoot = document.createElement('tfoot');
+    tableElem.appendChild(tableFoot);
+
+    //FOOTER ROW
+    const footerRow = document.createElement('tr');
+    tableFoot.appendChild(footerRow);
+
+    //CREATES "TOTALS" LABEL FOR HOURLY TOTALS
+    const dayTotal = document.createElement('th');
+    footerRow.appendChild(dayTotal);
+    dayTotal.textContent = 'Totals';
+
+    //INSERTS HOURLY TOTALS INTO FOOTER TAGS
+    for (let i = 0; i < header.length; i++) {
+        const footerCell = document.createElement('th');
+        footerRow.appendChild(footerCell);
+        footerCell.textContent = hourlyTotals[i];
+    }
+
+    //GRAND TOTAL IN LAST CELL
+    const grandTCell = document.createElement('th');
+    footerRow.appendChild(grandTCell);
+    grandTCell.textContent = grandTotal;
+};
+
+createTable();  //ONLY NEEDS TO RUN ONCE
+
+// FILLS THE TABLE ROWS WITH STORE DATA
+Location.prototype.render3 = function () {
+
+    // STARTING POINT ATTACHES TABLE BODY TO TABLE                                              
+    const tableBody = document.createElement('tbody');
+    tableElem.appendChild(tableBody);
+
+    const dataRow = document.createElement('tr');
+    tableBody.appendChild(dataRow);
+
+    const nameCell = document.createElement('th');
+    dataRow.appendChild(nameCell);
+    nameCell.textContent = this.title;
+
+    for (let i = 0; i < this.randCookiesPerH.length; i++) {
+        const dataCell = document.createElement('td');
+        dataRow.appendChild(dataCell);
+        dataCell.textContent = this.randCookiesPerH[i];
+    }
+
+    const totalCell = document.createElement('th');
+    dataRow.appendChild(totalCell);
+    totalCell.textContent = this.allCookiesSold;
+};
+
+//VALUES BY STORE IN A LIST
+Location.prototype.render = function () {     // This  puts data on the screen as a list! 
+
+    // starting point                                               // DOM!!!!  DOM!!!!
+    const containerElem = document.getElementById('locations');    // sets the root container of the function to the section with 'location' id?
+
+    // articles
+    const articleElem = document.createElement('article');          // creates an article element in articleElem
+    containerElem.appendChild(articleElem);                         // and appends in below the section element (containerElem)
+
+    // heading
+    const h2Elem = document.createElement('h2');                    // creates an h2 element.  H1 was already used in the page.
+    articleElem.appendChild(h2Elem);                                // and appends in below the article element
+    h2Elem.textContent = this.title;                                // pulls the title value string from the object
+
+    // list container                                               
+    const ulElem = document.createElement('ul');                    // creates a ul element.
+    articleElem.appendChild(ulElem);
+
+    // list item
+    for (let i = 0; i < this.randCookiesPerH.length; i++) {         // loop created to populate multiple line items.
+        const liElem = document.createElement('li');                    // creates an li element.
+        ulElem.appendChild(liElem);                                     // appended liElem below ulElem.
+        const timeSlot = header[i];                                          // this pulls the time value string from the hours array.
+        const cookiesSoldThisHour = this.randCookiesPerH[i];
+        liElem.textContent = `${timeSlot}: ${cookiesSoldThisHour}`;        // called a statement to fill the list item
+    }
+};
 
 const seattle = new Location('Seattle', 23, 65, 6.3);
 console.log(seattle);  // lower case...  why?
-seattle.render();
+seattle.render();  //renders the basic list
+seattle.render2();  //renders inputs into a table  **extra
+seattle.render3();  //renders outputs into a table
 
 const tokyo = new Location('Tokyo', 3, 24, 1.2);
 console.log(tokyo);
 tokyo.render();
+tokyo.render2();
+tokyo.render3();
 
 const dubai = new Location('Dubai', 11, 38, 3.7);
 console.log(dubai);
 dubai.render();
+dubai.render2();
+dubai.render3();
 
 const paris = new Location('Paris', 20, 38, 2.3);
 console.log(paris);
 paris.render();
+paris.render2();
+paris.render3();
 
 const lima = new Location('Lima', 2, 16, 4.6);
 console.log(lima);
 lima.render();
+lima.render2();
+lima.render3();
